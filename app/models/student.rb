92e50929c5a5bffbdf4dd2b0fd3dac7504d2490c
Habib <xphabib@gmail.com>
2019-11-05 01:45:27 +0600

    mentor and student model added

---
+++ app/models/student.rb
@@ -0,0 +1,35 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  first_name             :string
+#  last_name              :string
+#  phone                  :string
+#  city_id                :integer
+#  country_name           :string
+#  city_name              :string
+#  department_name        :string
+#  university             :string
+#  cgpa                   :float
+#  nationality            :string
+#  occupation             :string
+#  user_type              :string
+#  role                   :string
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+
+class Student < User
+
+end
