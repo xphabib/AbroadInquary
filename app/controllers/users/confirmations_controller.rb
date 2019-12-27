942e702fb1e8a542abd1d0bd89b82a1c0a01d51a
Habib <xphabib@gmail.com>
2019-12-28 00:01:15 +0600

    confirmable

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -15,14 +15,13 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   def show
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
     yield resource if block_given?
-    set_flash_message!(:notice, :confirmed)
-    redirect_to new_user_session_path
-    # if resource.errors.empty?
-    #   set_flash_message!(:notice, :confirmed)
-    #   respond_with_navigational(resource){ redirect_to new_user_session_path }
-    # else
-    #   respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
-    # end
+
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      respond_with_navigational(resource){ redirect_to new_user_session_path }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ redirect_to new_user_session_path }
+    end
   end
 
   # protected
