c8d8f848087960de9442f16406d982416e5957df
Habib <xphabib@gmail.com>
2020-03-10 00:46:00 +0600

    homepage country mentors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,14 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
   def index
     @countries = Country.all.order(:name)
+    @popular_countries = @countries.first(3)
+    @popular_country_mentors = @popular_countries.collect {|c| c.users.mentors.first}
+
+    p "<<<<<<<<<<<<<<<<<"
+    p @popular_countries
+    p @popular_country_mentors
+    p "<<<<<<<<<<<<<<<<<"
+
     @blogs = Blog.last(3)
   end
 
