bb4fe13c21f6ebc7bad007f5501961ee9a12d094
Habib <xphabib@gmail.com>
2020-03-29 23:16:02 +0600

    remove mail confirmable, user login and sign up in footer

--- app/models/user.rb
+++ app/models/user.rb
@@ -73,7 +73,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable#, :confirmable
   mount_uploader :image, AvatarUploader
   extend FriendlyId
   friendly_id :full_name, use: :slugged
